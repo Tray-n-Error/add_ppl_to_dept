fa44b0046ea41dc1e4b9b5ba46c8164c2b860d77
Tray-n-Error <schleytrajan@gmail.com>
2025-03-09 21:54:54 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@
 //employee names to a department in a company. For example,
 //Add Sally to Engineering or Add Amir to Sales. Then let the user
 //retrieve a list of all people in a department or all people in the company
-//by department, sorted alphabetically.
-
-//WITHOUT ANY HELP FROM ChatGPT !!!
+//by department.
 
 fn main() {
 	use std::collections::HashMap;
